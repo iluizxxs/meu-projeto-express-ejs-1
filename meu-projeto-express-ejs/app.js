c4695af7ecc84f0e94a1b0c9950366f38327d694
JCTSC <jctscamargo@gmail.com>
2023-09-20 13:57:42 -0400

    MeuProjetoExpress.js20.09

--- meu-projeto-express-ejs/app.js
+++ meu-projeto-express-ejs/app.js
@@ -6,7 +6,11 @@ app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Olá, Mundo!' });
+  res.render('index', { message: 'Mercado 1000 em 1!' });
+});
+
+app.get('/produtos', (req, res) => {
+  res.render('produtos', { message: 'Mercado 1000 em 1!' });
 });
 
 app.listen(port, () => {
