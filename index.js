5fb939e9463b7ad342dcffe179cce360355e52ed
iluizxx <Luiz.slva34@estudante.ifms.edu.br>
2023-11-16 16:44:22 -0400

    Aula 16/11

---
+++ index.js
@@ -0,0 +1,29 @@
+const produtos = [
+    {id: 1, nome: "Camisa do São Paulo", preco:"200", descricao:"Camisa do São Paulo"},
+    {id: 1, nome: "Camisa do Barcelona", preco:"200", descricao:"Camisa do Barcelona"},
+    {id: 1, nome: "Camisa do Psg", preco:"200", descricao:"Camisa do Psg"},
+    {id: 1, nome: "camisa do Palmeiras", preco:"200", descricao:"Camisa do Palmeiras"},
+    {id: 1, nome: "Camisa do Real Madrid", preco:"200", descricao:"Camisa do Real Madrid"},
+    {id: 1, nome: "Camisa do Manchester City", preco:"200", descricao:"Camisa do Manchester City"},
+    {id: 1, nome: "Camisa do Manchester United", preco:"200", descricao:"Camisa do"},
+    {id: 1, nome: "Camisa do Liverpool", preco:"200", descricao:"Camisa do Liverpool"},
+    {id: 1, nome: "Camisa do Bayern de Muniqiue", preco:"200", descricao:"Camisa do Bayern de Munique"},
+    {id: 1, nome: "Camisa do Atletico de Madrid", preco:"200", descricao:"Camisa do Atletico de Madrid"}
+  ]
+
+ 
+  }
+
+for (let produto of produtos){
+    console.log(produto)
+  }
+  
+  
+  function buscarProdutosPorId(id){
+    const produto = produtos.find(produto => produto.id = id);
+    return produto
+  }
+  
+  
+  let final =buscarProdutosPorId(2)
+  console.log(final)
--- meu-projeto-express-ejs/app.js
+++ meu-projeto-express-ejs/app.js
@@ -3,23 +3,28 @@ const app = express();
 const port = 3000;
 
 const produtos = [
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""},
-  {id: 1, nome: "", preco:"", descricao:""}
+  {id: 1, nome: "Camisa do São Paulo", preco:"200", descricao:"Camisa do São Paulo"},
+  {id: 1, nome: "Camisa do Barcelona", preco:"200", descricao:"Camisa do Barcelona"},
+  {id: 1, nome: "Camisa do Psg", preco:"200", descricao:"Camisa do Psg"},
+  {id: 1, nome: "camisa do Palmeiras", preco:"200", descricao:"Camisa do Palmeiras"},
+  {id: 1, nome: "Camisa do Real Madrid", preco:"200", descricao:"Camisa do Real Madrid"},
+  {id: 1, nome: "Camisa do Manchester City", preco:"200", descricao:"Camisa do Manchester City"},
+  {id: 1, nome: "Camisa do Manchester United", preco:"200", descricao:"Camisa do"},
+  {id: 1, nome: "Camisa do Liverpool", preco:"200", descricao:"Camisa do Liverpool"},
+  {id: 1, nome: "Camisa do Bayern de Muniqiue", preco:"200", descricao:"Camisa do Bayern de Munique"},
+  {id: 1, nome: "Camisa do Atletico de Madrid", preco:"200", descricao:"Camisa do Atletico de Madrid"}
 ]
+
+function buscarProdutosPorId(id){
+  const produto = produtos.find(produto => produto.id = id);
+  return produto
+}
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.render('index', { message: 'Mercado 1000 em 1!' });
+  res.render('index', {produtos});
 });
 
 app.get('/produtos', (req, res) => {
@@ -29,3 +34,4 @@ app.get('/produtos', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
 });
+
